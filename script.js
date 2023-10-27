6240088f9fb5c6d10e1454faa96c6618e2023ae3
Deepesh2023 <officialdeepeshsg@gmail.com>
2023-10-27 15:10:05 +0530

    fixed decimal repeating

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let initialNumber = 0;
 let finalNumber = 0;
 let operator;
 let displayNumber = "";
-const maxDisplayLength = 8;
+const maxTextLength = 8;
 const displayWidth = $(".calculatorDisplay").width();
+let decimalButtonPressedOnce = false;
+const reDot = /[.]/;
 
 $("p").append(displayNumber);
 
@@ -13,16 +15,17 @@ buttonResponse();
 function buttonResponse() {
   // the numbers
   $(".zero").click(function () {
-    if (displayNumber.length >= maxDisplayLength) {
+    if (displayNumber.length >= maxTextLength) {
       return;
     }
 
     displayNumber += "0";
     $("p").empty();
+    textResize();
     $("p").append(displayNumber);
   });
   $(".one").click(function () {
-    if (displayNumber.length >= maxDisplayLength) {
+    if (displayNumber.length >= maxTextLength) {
       return;
     }
 
@@ -32,7 +35,7 @@ function buttonResponse() {
     $("p").append(displayNumber);
   });
   $(".two").click(function () {
-    if (displayNumber.length >= maxDisplayLength) {
+    if (displayNumber.length >= maxTextLength) {
       return;
     }
 
@@ -42,7 +45,7 @@ function buttonResponse() {
     $("p").append(displayNumber);
   });
   $(".three").click(function () {
-    if (displayNumber.length >= maxDisplayLength) {
+    if (displayNumber.length >= maxTextLength) {
       return;
     }
 
@@ -52,7 +55,7 @@ function buttonResponse() {
     $("p").append(displayNumber);
   });
   $(".four").click(function () {
-    if (displayNumber.length >= maxDisplayLength) {
+    if (displayNumber.length >= maxTextLength) {
       return;
     }
 
@@ -62,7 +65,7 @@ function buttonResponse() {
     $("p").append(displayNumber);
   });
   $(".five").click(function () {
-    if (displayNumber.length >= maxDisplayLength) {
+    if (displayNumber.length >= maxTextLength) {
       return;
     }
 
@@ -72,7 +75,7 @@ function buttonResponse() {
     $("p").append(displayNumber);
   });
   $(".six").click(function () {
-    if (displayNumber.length >= maxDisplayLength) {
+    if (displayNumber.length >= maxTextLength) {
       return;
     }
 
@@ -82,7 +85,7 @@ function buttonResponse() {
     $("p").append(displayNumber);
   });
   $(".seven").click(function () {
-    if (displayNumber.length >= maxDisplayLength) {
+    if (displayNumber.length >= maxTextLength) {
       return;
     }
 
@@ -92,7 +95,7 @@ function buttonResponse() {
     $("p").append(displayNumber);
   });
   $(".eight").click(function () {
-    if (displayNumber.length >= maxDisplayLength) {
+    if (displayNumber.length >= maxTextLength) {
       return;
     }
 
@@ -102,7 +105,7 @@ function buttonResponse() {
     $("p").append(displayNumber);
   });
   $(".nine").click(function () {
-    if (displayNumber.length >= maxDisplayLength) {
+    if (displayNumber.length >= maxTextLength) {
       return;
     }
 
@@ -113,9 +116,14 @@ function buttonResponse() {
   });
 
   $("#decimal").click(function () {
-    if (displayNumber.length >= maxDisplayLength) {
+    if (displayNumber.length >= maxTextLength) {
       return;
     }
+    if (decimalButtonPressedOnce == true) {
+      return;
+    }
+
+    decimalButtonPressedOnce = true;
 
     displayNumber += ".";
     $("p").empty();
@@ -125,38 +133,24 @@ function buttonResponse() {
 
   //   operations
   $("#A").click(function () {
-    initialNumber = +displayNumber;
     operator = "A";
-    $("p").empty();
-    displayNumber = "";
+    setInitialValue();
   });
   $("#S").click(function () {
-    initialNumber = +displayNumber;
     operator = "S";
-    $("p").empty();
-    displayNumber = "";
+    setInitialValue();
   });
   $("#M").click(function () {
-    initialNumber = +displayNumber;
     operator = "M";
-    $("p").empty();
-    displayNumber = "";
+    setInitialValue();
   });
   $("#D").click(function () {
-    initialNumber = +displayNumber;
     operator = "D";
-    $("p").empty();
-    displayNumber = "";
+    setInitialValue();
   });
 
   $("#result").click(function () {
-    // if (displayNumber.length == maxDisplayLength) {
-    //   return;
-    // }
-    finalNumber = +displayNumber;
-    $("p").empty();
-    performingOperation(initialNumber, finalNumber, operator);
-    finalNumber = 0;
+    performingOperation(initialNumber, operator);
   });
 
   // clearing the numbers
@@ -164,6 +158,7 @@ function buttonResponse() {
     displayNumber = displayNumber.slice(0, -1);
     $("p").empty();
     $("p").append(displayNumber);
+    checkForDecimalPoints();
   });
 
   $("#allClear").click(function () {
@@ -172,25 +167,42 @@ function buttonResponse() {
     initialNumber = 0;
     finalNumber = 0;
     operator = "";
+    decimalButtonPressedOnce = false;
   });
 }
 
-function performingOperation(value1, value2, operation) {
-  switch (operation) {
+function setInitialValue() {
+  initialNumber = +displayNumber;
+  $("p").empty();
+  displayNumber = "";
+  decimalButtonPressedOnce = false;
+}
+
+function performingOperation() {
+  finalNumber = +displayNumber;
+  $("p").empty();
+  switch (operator) {
     case "A":
-      initialNumber = value1 + value2;
+      initialNumber = initialNumber + finalNumber;
       break;
     case "S":
-      initialNumber = value1 - value2;
+      initialNumber = initialNumber - finalNumber;
       break;
     case "M":
-      initialNumber = value1 * value2;
+      initialNumber = initialNumber * finalNumber;
       break;
     case "D":
-      initialNumber = value1 / value2;
+      initialNumber = initialNumber / finalNumber;
       break;
   }
   displayNumber = initialNumber.toString();
+  finalNumber = 0;
+  if (displayNumber.length > 20) {
+    alert("Cannot display such large numbers.");
+    location.reload();
+    return;
+  }
+  checkForDecimalPoints();
   $("p").append(displayNumber);
   textResize();
   return;
@@ -199,11 +211,7 @@ function performingOperation(value1, value2, operation) {
 // resizing the text to acomedate the length
 function textResize() {
   let textWidth = $("p").width();
-  if (textWidth > (180 / 100) * displayWidth) {
-    displayNumber = "";
-    alert("Cannot display such large numbers.");
-    location.reload();
-  } else if (textWidth > (150 / 100) * displayWidth) {
+  if (textWidth > (150 / 100) * displayWidth) {
     $(".calculatorDisplay").css("fontSize", "2em");
     return;
   } else if (textWidth > (120 / 100) * displayWidth) {
@@ -218,3 +226,11 @@ function textResize() {
   return;
 }
 
+function checkForDecimalPoints() {
+  if (displayNumber.search(reDot) == -1) {
+    decimalButtonPressedOnce = false;
+    return;
+  }
+  decimalButtonPressedOnce = true;
+  return;
+}
